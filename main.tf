a5143855b371666871fe892e7d61615a29dbc8ea
Dinusha <dinusha@Dinushas-MacBook-Pro.local>
2022-08-19 00:39:04 +0800

    Terraform first tutorial

---
+++ main.tf
@@ -0,0 +1,16 @@
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+    name = "nginx:latest"
+    keep_locally = false
+  
+}
+
+resource "docker_container" "nginx" {
+    image = docker_image.nginx.latest
+    name = "tutorial1"
+    ports {
+      internal = 80
+      external = 8000
+    }
+}
