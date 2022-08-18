ab8242310e2551530fd8b4423252e8a8861d0e10
Dinusha Marathunga <dinusha123pradeep@gmail.com>
2022-08-19 01:07:49 +0800

    Added useful comments into the terraform block and ran terraform fmt

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 provider "docker" {}
 
 resource "docker_image" "nginx" {
-    name = "nginx:latest"
-    keep_locally = false
-  
+  name         = "nginx:latest"
+  keep_locally = false
+
 }
 
 resource "docker_container" "nginx" {
-    image = docker_image.nginx.latest
-    name = "tutorial1"
-    ports {
-      internal = 80
-      external = 8000
-    }
+  image = docker_image.nginx.latest
+  name  = "tutorial1"
+  ports {
+    internal = 80
+    external = 8000
+  }
 }
